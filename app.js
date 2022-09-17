9d7bd153d5f3c446b585f55ad99880289fd182f0
Mr_Cheeezz <skylerspear26@gmail.com>
2022-09-17 01:53:50 -0400

    V2

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const client = new tmi.Client({
 client.connect();
 
 client.on("connected", (channel, username, viewers, method) => {
-    // client.say(CHANNEL_NAME, `/me [🤖]: Joined channel ${CHANNEL_NAME}. aly1263Minion`)
+    client.say(CHANNEL_NAME, `/me [🤖]: Joined channel ${CHANNEL_NAME}. aly1263Minion`)
 });
 
 setInterval(async () => {
